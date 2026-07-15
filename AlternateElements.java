3a13f0bba4b529fa2f09cb5aee1d8456c18393c0
miruthula <miruthula183@gmail.com>
2026-07-15 10:24:24 -0700

    Add program to print alternate elements

---
+++ AlternateElements.java
@@ -0,0 +1,12 @@
+public class AlternateElements {
+
+    public static void main(String[] args) {
+
+        int[] arr={10,20,30,40,50,60};
+
+        for(int i=0;i<arr.length;i+=2){
+
+            System.out.println(arr[i]);
+        }
+    }
+}
