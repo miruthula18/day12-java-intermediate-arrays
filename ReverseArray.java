84ad2d9382142eade3b30eda9c76bf62dff7fc6a
miruthula <miruthula183@gmail.com>
2026-07-15 10:26:48 -0700

    Add program to reverse an array

---
+++ ReverseArray.java
@@ -0,0 +1,9 @@
+public class ReverseArray {
+    public static void main(String[] args){
+        int[] arr={10,47,29,48,49};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
